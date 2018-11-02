3f6081463f9efab52156f5ce2e255dec6f6edba2
shikibum <satoshikibu@gmail.com>
2018-11-02 16:36:05 +0900

    .gitignoreに/node_modulesを追加

---
+++ 001.js
@@ -0,0 +1,20 @@
+if (true) {
+  var x = 5;
+}
+console.log(x)
+
+if (true) {
+  let y = 5;
+}
+console.log(y);
+// ReferenceError: y is not defined
+
+var answer = 42;
+answer = 'Thanks for all the fish...';
+console.log(answer)
+
+x = '答えは ' + 42 // "答えは 42"
+y = 42 + ' が答え' // "42 が答え"
+
+'37' - 7 // 30
+  '37' + 7 // "377"
